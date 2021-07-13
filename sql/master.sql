784eaf4d8fa6e2cb97490a020e90c48725787b72
Brandon Heulga <bahuelga@gmail.com>
2021-07-13 11:50:55 -0700

    added node packages

---
+++ sql/master.sql
@@ -0,0 +1,8 @@
+/*
+ * Master SQL
+ - - - - - - - -
+ This file describes the most up-to-date database schema.
+ When you add a new SQL script for a feature to the "updates" directory,
+ make those same changes here, so we always have an up-to-date master
+ create file.
+ */
